9ae877e42d2c49bd7ca0d45b94b1ef5c96a76a66
David Arce <arce475@gmail.com>
2024-04-13 22:58:45 -0300

    envio

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cron = require('node-cron');
-const nodemailer = require('nodemailer'); // Importa la biblioteca de Nodemailer
-const moment = require('moment-timezone'); // Importa moment-timezone
+const nodemailer = require('nodemailer');
+const moment = require('moment-timezone');
 require('dotenv').config();
 
-// Configura la zona horaria para Argentina
 moment.tz.setDefault('America/Argentina/Buenos_Aires');
 
 const app = express();
@@ -16,11 +15,12 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
     .then(() => console.log('Conexión a MongoDB establecida'))
     .catch((err) => console.error('Error al conectar a MongoDB:', err));
 
-// Configura el transporte de correo con Nodemailer
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
@@ -40,10 +40,9 @@ const turnoSchema = new mongoose.Schema({
 
 const Turno = mongoose.model('Turno', turnoSchema);
 
-// Función para enviar correo electrónico al cliente con la fecha y hora del turno
-const enviarCorreoElectronico = async (fechaFormateada) => {
+const enviarCorreoElectronico = async (cliente, fechaFormateada, tipoServicio) => {
     try {
-        const mensaje = `${cliente} Tu turno ha sido reservado para el ${fechaFormateada} para el servicio ${tipoServicio}.`;
+        const mensaje = `${cliente}, tu turno ha sido reservado para el ${fechaFormateada} para el servicio ${tipoServicio}.`;
         await transporter.sendMail({
             from: process.env.EMAIL_USER,
             to: process.env.EMAIL_CONTACTO,
@@ -56,7 +55,6 @@ const enviarCorreoElectronico = async (fechaFormateada) => {
     }
 };
 
-// Función para eliminar turnos pasados de la fecha actual
 const eliminarTurnosPasados = async () => {
     const fechaActual = new Date();
     try {
@@ -67,15 +65,14 @@ const eliminarTurnosPasados = async () => {
     }
 };
 
-// Función para obtener horarios disponibles para nuevos turnos
 const obtenerHorariosDisponibles = async () => {
     const turnos = await Turno.find();
-    const horariosReservados = turnos.map(turno => turno.fechaHora.getHours() + ':' + turno.fechaHora.getMinutes());
+    const horariosReservados = turnos.map(turno => moment(turno.fechaHora).format('HH:mm'));
     const horariosDisponibles = [];
 
     for (let hora = 8; hora <= 20; hora++) {
         for (let minuto of ['00', '30']) {
-            const horario = hora + ':' + minuto;
+            const horario = `${hora}:${minuto}`;
             if (!horariosReservados.includes(horario)) {
                 horariosDisponibles.push(horario);
             }
@@ -85,8 +82,7 @@ const obtenerHorariosDisponibles = async () => {
     return horariosDisponibles;
 };
 
-// Configurar el cron job para ejecutar la función cada día a la medianoche
-cron.schedule('*/30 * * * *', eliminarTurnosPasados);
+cron.schedule('0 0 * * *', eliminarTurnosPasados);
 
 app.get('/turnos', async (req, res) => {
     try {
@@ -107,7 +103,7 @@ app.get('/horarios-disponibles', async (req, res) => {
 });
 
 app.post('/turnos/reservar', async (req, res) => {
-    const { fechaHora } = req.body;
+    const { fechaHora, nombreCliente, tipoServicio } = req.body;
 
     try {
         const turnoExistente = await Turno.findOne({ fechaHora });
@@ -116,24 +112,19 @@ app.post('/turnos/reservar', async (req, res) => {
             return res.status(400).json({ message: 'El turno para esta fecha y hora ya está reservado' });
         }
 
-        const nuevoTurno = new Turno({ fechaHora });
+        const nuevoTurno = new Turno({ fechaHora, nombreCliente, tipoServicio });
         await nuevoTurno.save();
 
-        // Formatear la fecha y hora en la zona horaria deseada (Argentina)
         const fechaFormateada = moment(fechaHora).format('DD-MM-YYYY HH:mm');
+        await enviarCorreoElectronico(nombreCliente, fechaFormateada, tipoServicio);
 
-        // Enviar correo electrónico al cliente con la fecha y hora del turno
-        await enviarCorreoElectronico(fechaFormateada); // Pasar la fecha formateada a la función
-
-        // Después de guardar el nuevo turno y enviar el correo electrónico, obtener los horarios disponibles actualizados
         const horariosDisponiblesActualizados = await obtenerHorariosDisponibles();
         res.status(201).json({ message: 'Turno reservado exitosamente', horariosDisponibles: horariosDisponiblesActualizados });
     } catch (err) {
-        console.error('Error al reservar el turno:', err); // Registrar el error en los registros del servidor
-        res.status(500).json({ message: 'Error interno al procesar la solicitud' }); // Devolver una respuesta descriptiva al cliente
+        console.error('Error al reservar el turno:', err);
+        res.status(500).json({ message: 'Error interno al procesar la solicitud' });
     }
 });
-
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
