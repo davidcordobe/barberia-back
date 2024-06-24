e4d96cdec24eed55d865e6e6c3ad6f449ed651b8
david arce <arce475@gmail.com>
2024-06-23 23:23:01 -0300

    email masche

--- db.js
+++ db.js
@@ -1,24 +1,30 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-let connection;
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+
 
 const connectToDatabase = async () => {
-    if (!connection) {
-        try {
-            connection = await mysql.createConnection({
-                host: process.env.DB_HOST,
-                user: process.env.DB_USER,
-                password: process.env.DB_PASSWORD,
-                database: process.env.DB_NAME,
-            });
-            console.log('Conexión a la base de datos exitosa');
-        } catch (error) {
-            console.error('Error al conectar a la base de datos:', error);
-            throw error;
-        }
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        });
+        console.log('Conexión a la base de datos exitosa');
+        return connection;
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        throw error;
     }
-    return connection;
 };
+
+connectToDatabase();
+
+
 
 module.exports = connectToDatabase;
